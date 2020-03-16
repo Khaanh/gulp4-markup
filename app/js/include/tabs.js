de8d11bcc42939143da3fc3fef7299bfbb99d3d9
Khanh <Khanh13Nguyen13@gmail.com>
2020-03-16 19:39:43 +0200

    Add tabs js

--- app/js/include/tabs.js
+++ app/js/include/tabs.js
@@ -1,30 +1,7 @@
-let tabLinks = document.querySelectorAll('.tablinks'),
-    tabContent = document.querySelectorAll('.tabcontent');
-
-tabLinks.forEach(function(elem) {
-  elem.addEventListener('click', openTabs);
-});
-
-function openTabs(elem) {
-  let btnTarget = elem.currentTarget,
-      country = btnTarget.dataset.country;
-
-      tabContent.forEach(function(elem) {
-        elem.classList.remove('active');
-      });
-
-      tabLinks.forEach(function(elem) {
-        elem.classList.remove("active");
-     });
-
-     document.querySelectorAll('#' + country).classList.add('active');
-
-     btnTarget.classList.add('active');
-}
-
-
-function plus(a,b) {
-  console.log(a + b);
-}
-
-plus(4,6);
+$('.tabslink').on('click', function(e) {
+  let id = $(this).attr('href')
+  $(this).closest('.tabs').siblings('.holder-content').find('.tabcontent').removeClass('active')
+  $(this).addClass('active').siblings('.tabslink').removeClass('active')
+  $('#' + id).addClass('active')
+  e.preventDefault();
+})
