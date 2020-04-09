9616ea46ad70e4223ec918979067b45019a420dd
Khaanh <35469637+Khaanh@users.noreply.github.com>
2020-04-09 20:44:34 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
 $(function() {
+  //= include/range-slider
+	//= include/markuplist
+  //= include/accordion
 	//= include/default
+	//= include/sliders
 	//= include/popup
   //= include/forms
+	//= include/tabs
 	
 });
 
