734e35d54a6578ddb3f13540e64310ccac66cdf4
Khanh <Khanh13Nguyen13@gmail.com>
2020-07-07 22:24:33 +0300

    Remove tabs-elem from starter

--- app/js/include/tabs.js
+++
@@ -1,7 +0,0 @@
-$('.tabslink').on('click', function(e) {
-  let id = $(this).attr('href')
-  $(this).closest('.tabs').siblings('.holder-content').find('.tabcontent').removeClass('active')
-  $(this).addClass('active').siblings('.tabslink').removeClass('active')
-  $('#' + id).addClass('active')
-  e.preventDefault();
-})
